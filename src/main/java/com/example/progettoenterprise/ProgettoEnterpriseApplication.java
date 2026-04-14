76a291461a113eebf3a5e15850b6aff4c250ec77
darioooooa <dario2004dp@gmail.com>
2026-04-14 14:15:25 +0200

    Creazione progetto

---
+++ src/main/java/com/example/progettoenterprise/ProgettoEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettoenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoEnterpriseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/progettoenterprise/ProgettoEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettoenterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoEnterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
