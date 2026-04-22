3362a2bb7a532176e02d63916c0362250a64bbd8
Lorenzobarcollo19 <lorenzointrieri05@gmail.com>
2026-04-23 00:50:12 +0200

    Creata schermataOrganizzatore creata la rotta e creazione della repository service per collegamenti con i controller

---
+++ frontendWeb/src/app/schermataOrganizzatore/schermataOrganizzatore.ts
@@ -0,0 +1,43 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-schermata-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './schermataOrganizzatore.html',
+  styleUrl: './schermataOrganizzatore.css'
+})
+export class SchermataOrganizzatoreComponent {
+  listaDeiViaggi: any[] = [];
+  ngOnInit(): void {
+    this.caricaViaggi();
+  }
+
+  caricaViaggi() {
+    // Esempio manuale per testare se il rosso sparisce:
+    this.listaDeiViaggi = [
+      { destinazione: 'Bali', prezzo: 1200 },
+      { destinazione: 'Sahara', prezzo: 850 }
+    ];
+  }
+  modifica(viaggio: any) {
+    console.log("Voglio modificare il viaggio:", viaggio);
+
+    alert("Funzione modifica per: " + viaggio.destinazione);
+  }
+
+  // Funzione per l'eliminazione
+  elimina(viaggio: any) {
+    if(confirm("Sei sicuro di voler eliminare " + viaggio.destinazione + "?")) {
+      console.log("Elimino il viaggio con ID:", viaggio.id);
+
+
+      this.listaDeiViaggi = this.listaDeiViaggi.filter(v => v !== viaggio);
+    }
+  }
+  apriModalCreazione() {
+    alert("Qui si aprirebbe il form per il nuovo viaggio!");
+  }
+
+}
